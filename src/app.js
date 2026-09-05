a2ee8defd81565c86b81f5469b0b47952bc42b01
bebodongol <bebodongol@gamil.com>
2026-09-05 09:28:58 +0300

    Creates anew NodeJS app

---
+++ src/app.js
@@ -0,0 +1,10 @@
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+module.exports = greet; //ensure the function is exported for use in other files
+//if run directly, print output (useful for debugging)
+
+if (require.main === module) {
+    console.log(greet('World'));
+}
